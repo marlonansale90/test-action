40a9cceb7490a81bb5d25a6e8f32fcd96b91f349
marlonvon90 <marlonvon.ansale@fanduel.com>
2023-02-10 14:19:08 +0000

    Added Project for testing

---
+++ AnyAppTest/AnyAppTest/AnyAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyAppTestApp.swift
+//  AnyAppTest
+//
+//  Created by Marlon Von Bernales Ansale on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnyAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnyAppTest/AnyAppTestTests/AnyAppTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  AnyAppTestTests.swift
+//  AnyAppTestTests
+//
+//  Created by Marlon Von Bernales Ansale on 10/02/2023.
+//
+
+import XCTest
+@testable import AnyAppTest
+
+final class AnyAppTestTests: XCTestCase {
+    
+    func testExample() throws {
+        XCTAssertEqual(3*3, 9)
+    }
+}
